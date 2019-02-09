ba379c137945a7fbfd50c5a271f5a90f246fb214
BobCochran <r2cochran2@gmail.com>
2019-02-09 18:41:34 -0500

    Set up project for reading ds18b20 temperature sensor data

---
+++ ds18b20_read/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ds18b20_read
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
